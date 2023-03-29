90c73c7f330642bb55bd0f970991092fe0ad074c
YaroslavBugayov <bugayov576@gmail.com>
2023-03-29 21:56:47 +0300

    animation abstraction

---
+++ Assets/Scripts/Player/PlayerAnimation/UnityAnimatorController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    [RequireComponent(typeof(Animator))]
+    public class UnityAnimatorController : AnimatorController
+    {
+        private Animator _animator;
+
+        private void Start() => _animator = GetComponent<Animator>();
+        
+        
+    }
+}
