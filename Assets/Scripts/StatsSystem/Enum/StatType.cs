058204d483df62e651f6e728408a99e80a402b1b
YaroslavBugayov <bugayov576@gmail.com>
2023-03-31 21:48:34 +0300

    stats system

---
+++ Assets/Scripts/StatsSystem/Enum/StatType.cs
@@ -0,0 +1,10 @@
+﻿namespace StatsSystem.Enum
+{
+    public enum StatType
+    {
+        None = 0,
+        Health = 1,
+        Speed = 2,
+        InventoryWeight = 3
+    }
+}
---
+++ Assets/Scripts/StatsSystem/StatsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Services.Updater;
+using StatsSystem.Enum;
+using UnityEngine;
+
+namespace StatsSystem
+{
+    public class StatsController : IDisposable, IStatValueGiver
+    {
+        private readonly List<Stat> _currentStats;
+        private readonly List<StatModificator> _activeModificators;
+
+        public StatsController(List<Stat> currentStats)
+        {
+            _currentStats = currentStats;
+            _activeModificators = new List<StatModificator>();
+            ProjectUpdater.Instanse.UpdateCalled += OnUpdate;
+        }
+
+        public float GetStatValue(StatType statType) => _currentStats.Find(stat => stat.Type == statType);
+
+        public void ProcessModificator(StatModificator modificator)
+        {
+            var statToChange = _currentStats.Find(stat => stat.Type == modificator.Stat.Type);
+            if (statToChange == null) 
+                return;
+
+            var addedValue = modificator.StatModificatorType == StatModificatorType.Additive
+                ? statToChange + modificator.Stat
+                : statToChange * modificator.Stat;
+            
+            statToChange.SetStatValue(statToChange + addedValue);
+            if (modificator.Duration < 0 )
+                return;
+
+            if (_activeModificators.Contains(modificator))
+            {
+                _activeModificators.Remove(modificator);
+            }
+            else
+            {
+                var addedStat = new Stat(modificator.Stat.Type, -addedValue);
+                var tempModificator = new StatModificator(addedStat, StatModificatorType.Additive, modificator.Duration,
+                    Time.time);
+                _activeModificators.Add(tempModificator);
+            }
+        }
+
+        private void OnUpdate()
+        {
+            if (_activeModificators.Count == 0)
+                return;
+
+            var expiredModificator = _activeModificators.Where(modificator =>
+                modificator.StartTime + modificator.Duration >= Time.time);
+
+            foreach (var modificator in expiredModificator)
+                ProcessModificator(modificator);
+        }
+
+        public void Dispose() => ProjectUpdater.Instanse.UpdateCalled -= OnUpdate;
+    }
+}
