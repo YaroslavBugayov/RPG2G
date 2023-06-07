d512af01c05f417e0dce44990e43b410c346f8b7
YaroslavBugayov <bugayov576@gmail.com>
2023-06-07 21:29:32 +0300

    tree system

--- Assets/Scripts/Environment/Tree.cs
+++ Assets/Scripts/Environment/Tree.cs
@@ -1,3 +1,4 @@
+using System;
 using Core.Services.Locator;
 using Items;
 using Items.Enum;
@@ -10,15 +11,32 @@ namespace Environment
         [SerializeField] private ServiceLocator _serviceLocator;
     
         private IItemGenerator _dropGenerator;
+        private bool _hasHarvest = true;
+        private Sprite _hasHarvestSprite;
+        private Sprite _hasNotHarvestSprite;
+        private SpriteRenderer _spriteRenderer;
+
+        private void Awake()
+        {
+            _hasHarvestSprite = Resources.Load<Sprite>("SpriteStates/Tree/TreeHasHarvest");
+            _hasNotHarvestSprite = Resources.Load<Sprite>("SpriteStates/Tree/TreeHasNotHarvest");
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
 
         private void Start()
         {
             _dropGenerator = _serviceLocator.GetService<IItemGenerator>();
         }
 
-        public void Harvest() 
+        public void Harvest()
         {
-            _dropGenerator.DropItem(ItemType.Apple, transform.position);
+            if (_hasHarvest)
+            {
+                _dropGenerator.DropItem(ItemType.Apple, transform.position);
+                _spriteRenderer.sprite = _hasNotHarvestSprite;
+            }
+
+            _hasHarvest = false;
         }
     }
 }
